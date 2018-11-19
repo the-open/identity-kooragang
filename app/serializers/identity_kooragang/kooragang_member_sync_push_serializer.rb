33ddad3086e22bd018a2e35fe9f9c05d4cc7b16d
Benjamin Mort <benm@getup.org.au>
2018-11-19 11:17:51 +1100

    handle first name when null in serializer

--- app/serializers/identity_kooragang/kooragang_member_sync_push_serializer.rb
+++ app/serializers/identity_kooragang/kooragang_member_sync_push_serializer.rb
@@ -6,6 +6,10 @@ module IdentityKooragang
       @object.id
     end
 
+    def first_name
+      @object.first_name ? @object.first_name : ''
+    end
+
     def phone_number
       @object.send(instance_options[:phone_type])
     end
