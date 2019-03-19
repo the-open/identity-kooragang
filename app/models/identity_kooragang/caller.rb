f2d41a74ed74834a55924fd432bbdcabfbed1008
B.J. Rossiter <b.j.rossiter@gmail.com>
2019-03-20 10:31:08 +1100

    Fix to constant loading error

--- app/models/identity_kooragang/caller.rb
+++ app/models/identity_kooragang/caller.rb
@@ -3,6 +3,6 @@ module IdentityKooragang
     include ReadOnly
     self.table_name = "callers"
     has_many :calls
-    belongs_to :team, optional: true
+    belongs_to :team, optional: true, class_name: 'IdentityKooragang::Team'
   end
 end
